136790a8f3bc85de8fb0f10a130305b9f27d6fec
rafiauliapermana <72639854+rafiauliapermana@users.noreply.github.com>
2022-03-10 00:13:09 +0700

    Add files via upload

---
+++ NO2.py
@@ -0,0 +1,10 @@
+# Nama  : Rafi Aulia Permana
+# NIM   : 20051397071
+# Prodi : D4 Manajemen Informatika (2020A)
+
+print ("Tabel Perkalian :")
+
+for n in range (1,11):
+    for m in range (1,11):
+        print (n * m, end = "\t")
+print ("\n")    
